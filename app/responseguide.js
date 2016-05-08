af4b786b94c1aefc30068d22a845510982f63434
bluecollarcoder <wayne@bluecollarcoder.com>
2016-05-08 04:49:35 -0400

    Add more suggested responses.

--- app/responseguide.js
+++ app/responseguide.js
@@ -4,14 +4,17 @@ var rules = [{
   key : 'fear',
   minDiff : 0.2,
   responses : [
-    'Don\'t worry, I can help you with this.'
+    'Don\'t worry, I can help you with this.',
+    'Stay with me...you can\'t freak out on my now.',
+    'Have no fear. You and I will make it through this together.'
   ]
 }, {
   key : 'anger',
   minDiff : 0.2,
   responses : [
     'I\'m sorry to upset you. Let me try again to help you.',
-    'I can see that you are getting upset. Let me try to see if we can find another solution to this problem.'
+    'I can see that you are getting upset. Let me try to see if we can find another solution to this problem.',
+    'I know this is frustrating. Let\'s give it another go.'
   ]
 }, {
   key : 'sadness',
@@ -19,6 +22,12 @@ var rules = [{
   responses : [
     'I\'m sorry to upset you. Let me know what I can do to help.',
     'I understand that it is a difficult situation, I will try my best to help.'
+  ]
+}, {
+  key : 'disgust',
+  minDiff : 0.2,
+  responses : [
+    'I\'m sorry to upset you. Let\'s try and see if we can make things better.'
   ]
 }];
 
